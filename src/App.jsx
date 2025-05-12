5b2891419cd2a82b6456ff4bd346f7e8ca97e4df
Arghya <arghyadeepshee00@gmail.com>
2025-05-12 14:00:00 +0530

    page not found error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
